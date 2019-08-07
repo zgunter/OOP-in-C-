5270092b52cc8b6dad6d2c4671ab60bb2843d21d
zgunter <53876445+zgunter@users.noreply.github.com>
2019-08-07 19:45:50 +0100

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,132 +1,133 @@
-// Simple code to store courses using vectors and strings
+// PHYS 30762 Programming in C++
+// Assignment 4
+
+// Simple demonstration of a C++ class
+
+// Hubble types: E[0-7], S0, S[a-c], SB[a-c], Irr
+// Redshift z in range [0,10]
+// Total mass M_tot in range [1e7,1e12] M_sun
+// Stellar mass fraction f_* in range [0,0.05]
 
 #include<iostream>
 #include<string>
-#include<sstream>
 #include<vector>
-#include<algorithm>
-
+#include<cmath>
+#include<string>
 using namespace std;
 
-int main(void)
+class galaxy {
+
+	//Private data
+private:
+	string Hubble_type;
+	double Redshift;
+	double Mass_total;
+	double Stella_ratio;
+	vector<string> Satellites;
+
+	//Public data
+public:
+
+	//Default constructor - Setting all paramaters to zero
+	galaxy() : Hubble_type{ "unknown" }, Redshift{ 0 }, Mass_total{ 0 }, Stella_ratio{ 0 } {}
+
+	//Parameterized constructor
+	galaxy(string type, double z, double M_tot, double Stella_r) :
+		Hubble_type{ type }, Redshift{ z }, Mass_total{ M_tot }, Stella_ratio{ Stella_r } {}
+
+	//Destructor
+	~galaxy() {;}
+
+	//Define member functions
+	//function to check for valid inputs
+	bool input_check();
+	//Output functions to print out data
+	void output_data();//define the function outside the class
+	//Function to calculate the stella mass
+	double Stellamass() { return Stella_ratio*Mass_total; }
+	//Function to change the Hubble type
+	void ChangeType(string type_new) {Hubble_type = type_new;}
+	//Fuction to add satellite information to galaxies
+	void Add_Sat(galaxy);
+};
+
+//Output function defined outside the class
+void galaxy::output_data()
+{	cout.precision(4);
+	cout << "Galaxy is of type " << Hubble_type << ", with redshift (z) value of " << Redshift
+		<< ", It has a total mass of " << Mass_total << " in units of solar mass, with a stella mass fraction of "
+		<< Stella_ratio << "."<< endl;
+}
+
+//input checks
+bool galaxy::input_check()
 {
-	const string prefix("PHYS ");
-	// Gather list of courses and their codes from user,
-	// storing data as a vector of strings
-	bool not_finished(true);
-	//create vector to store string types
-	vector<string> courses;
-	do
-	{	//declare variables
-		string degreecode; 
-		cout << "Please enter a course name (or press x if you have finished entering courses): \n";
-		//store input in the vector of strings
-		getline(cin, degreecode);
-		courses.push_back(degreecode);
-		//stop loop if user inputs x
-		if (degreecode == "x"){
-			courses.pop_back();//delete the last element
-			not_finished = false;
-			break;
-		}
-		cout << degreecode << " have been stored. \n";
-	} while (not_finished);
+	if (Redshift < 0 || Redshift > 10) {
+		cout << "Redshift value needs to be between 0-10!" << endl;
+		cout<<"This galaxy of type "<<Hubble_type<< " will not be stored in the data vector"<<endl;
+		return false;
+	}
+	else if (Mass_total<1e7 || Mass_total > 1e12) {
+		cout << "Mass of the galaxy needs to be between 1e7-1e12!" << endl;
+		cout << "This galaxy of type " << Hubble_type << " will not be stored in the data vector" << endl;
+		return false;
+	}
+	else if (Stella_ratio < 0 || Stella_ratio > 0.05) {
+		cout << "Ratio of stella mass needs to be between 0-0.05!" << endl;
+		cout << "This galaxy of type " << Hubble_type << " will not be stored in the data vector" << endl;
+		return false;
+	}
+	else return true;
+}
 
-	// Print out full list of courses
-	//create iterators
-	vector<string>::iterator begin_all{ courses.begin() }, end_all{ courses.end() };
-	//de-reference iterators
-	vector<string>::iterator courses_unsorted;
-	cout << "courses you have entered are: " << endl;
-	for (courses_unsorted = begin_all; courses_unsorted < end_all; ++courses_unsorted) {
-		//use string stream to combine prefix and input string
-		ostringstream output;
-		output << prefix << *courses_unsorted;
-		string course_entered{ output.str() };
-		cout << course_entered << endl;
-		output.str("");//clear output content
-	}
+//Function to add satellite info
+void galaxy::Add_Sat(galaxy A) {
 
-	// Extract courses belonging to a certain year
-	string yearChoice;
-	cout << "Please enter year: 1, 2, 3 or 4 " << endl;
-	cin >> yearChoice;
-	//prevent error inputs
-	if (yearChoice != "1" && yearChoice != "2" && yearChoice != "3" && yearChoice != "4") {
-		cin.clear();
-		cout << "that's not a valid year!" << endl;
-		cout << "Please enter year: 1, 2, 3 or 4 " << endl;
-		cin >> yearChoice;
-	}
-	//New vector to store course from a specific year
-	vector<string> courses_by_year;
-		for (size_t i{ 0 }; i < courses.size(); ++i) {
-			if (courses[i].substr(0, 1) == yearChoice) {
-				//store into the year vector
-				courses_by_year.push_back(courses[i]);
-			}
-		}
-	//output all courses available in the users input year
-	cout << "List of courses available in year "<<yearChoice<< " are:" << endl;
-	//create iterators
-	vector<string>::iterator begin_year{ courses_by_year.begin() }, end_year{ courses_by_year.end() };
-	vector<string>::iterator year;
-	for (year = begin_year; year < end_year; ++year) {
-		cout << prefix << *year << endl;
-	}
+}
+
+// End of class and associated member functions
+
+//main programme
+
+int main()
+{	
+	//Declare vectors and all galaxy types
+	vector <galaxy> galaxy_data;
 	
-	//Ask the user for sort options
-	string sortOption;
-	cout << "Sort by course code or name: ";
-	cin >> sortOption;
-	//prevent error inputs
-	if (sortOption != "code" && sortOption != "name") {
-		cin.clear();
-		cout << "Please enter either 'code' or 'name' " << endl;
-		cin >> sortOption;
-	}
-	//declare vector and itterator
-	if (sortOption == "code") {
-		//declare itterator for sorted by course code
-		vector<string>::iterator begin_code{ courses_by_year.begin() }, end_code{ courses_by_year.end() };
-		sort(begin_code, end_code);
-		vector<string>::iterator code;
-		cout << "year " << yearChoice << " courses sorted by course code are: " << endl;
-		//output data
-		for (code = begin_code; code < end_code; ++code) {
-			cout <<prefix<< *code << endl;
-		}
-	}
-	else if (sortOption == "name"){
-		//extract name of the courses and store in separate vector
-		vector<string> coursename_presort;
-		for (size_t j{ 0 }; j < courses_by_year.size(); ++j) {
-			//store everything from after the course number till the end of the string
-			string coursenames = courses_by_year[j].substr(5, string::npos);
-			coursename_presort.push_back(coursenames);
-		}
-		//sort just the coure names by introducing iterators
-		vector<string>::iterator begin_name{ coursename_presort.begin() }, end_name{ coursename_presort.end() };
-		sort(begin_name, end_name);
-		vector<string>::iterator name;
-		for (name = begin_name; name < end_name; ++name) {
-			//use string stream to access iterator
-			ostringstream output_sorted;
-			output_sorted <<*name;
-			string course_sorted{ output_sorted.str() };
-			//look through the list and find the course which contains the course name
-			for (size_t k{ 0 }; k < courses_by_year.size(); ++k) {
-				if (courses_by_year[k].find(course_sorted) != std::string::npos) {
-					cout << prefix << courses_by_year[k] << endl;
-				}
-			}
-			output_sorted.str("");//clear output content
-		}
-	}
+	string typeA{ "E[0-7]" }, typeB{ "S0" }, typeC{ "S[a-c]" }, typeD{ "SB[a-c]" }, typeE{ "Irr" };
 	
-	//Umpty vectors
-	courses.clear();
-	courses_by_year.clear();
+	//example using the default constructor
+	galaxy A; // declare object with no parameters, calling default constructor
 
-	return 0;
+	//example using the parameterized constructor
+	galaxy B(typeA, 3, 1e12, 0.01);
+	galaxy C(typeC, 1, 7e8, 0.1);
+	galaxy D(typeD, 4, 8.5e11, 0.04);//The Milky Way
+	galaxy E(typeE, 10, 7.8e11, 0.02);
+
+	//create satellite galaxy for the Milky Way - Large Magelanic Cloud
+	galaxy LMC(typeD, 2, 1e11, 0.005);
+	D.Add_Sat(LMC);
+
+	//change the type of galaxy B from Irr to S0
+	B.ChangeType(typeB);
+	galaxy f(B);
+
+	//store data in vector
+	galaxy_data.push_back(A);
+	if(B.input_check()==true) galaxy_data.push_back(B);
+	if(C.input_check() == true) galaxy_data.push_back(C);
+	if (D.input_check() == true) galaxy_data.push_back(D);
+	if (E.input_check() == true) galaxy_data.push_back(E);
+	if (f.input_check() == true) galaxy_data.push_back(F);
+
+	//print out data using iterator
+	vector<galaxy>::iterator print_output;
+	for (print_output = galaxy_data.begin(); print_output < galaxy_data.end(); ++print_output) {
+		print_output->output_data();
+		//calculate stella mass
+		cout<< "Mass of the stars in the galaxy is " << print_output->Stellamass()
+			<< " solar masses." << endl;
+	}
+
 }
